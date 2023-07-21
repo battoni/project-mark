8ee5b4475d92654d86c4637fb7f8b0f590838e2b
Guilherme Battoni <guilherme.battoni@gmail.com>
2023-07-21 15:35:01 -0300

    feat(Types): Add COLOR_UI enum globally

---
+++ src/types/ui.ts
@@ -0,0 +1,25 @@
+enum UI_COLORS {
+  DEFAULT = 'default',
+  STRONG = 'strong',
+  INACTIVE = 'inactive',
+
+  ERROR = 'error',
+  ERROR_INACTIVE = 'errorInactive',
+  INFORMATION = 'information',
+  INFORMATION_INACTIVE = 'informationInactive',
+  SUCCESS = 'success',
+  SUCCESS_INACTIVE = 'successInactive',
+  WARNING = 'warning',
+  WARNING_INACTIVE = 'warningInactive',
+
+  BLUE = 'blue',
+  GRAY = 'gray',
+  GREEN = 'green',
+  ORANGE = 'orange',
+  PINK = 'pink',
+  PURPLE = 'purple',
+  SKY = 'sky',
+  WHITE = 'white',
+}
+
+export { UI_COLORS };
