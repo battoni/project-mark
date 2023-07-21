2a34c16973be3f11b7fcc04e25f3e69f18c12aca
Guilherme Battoni <guilherme.battoni@gmail.com>
2023-07-21 12:22:16 -0300

    feat(AppText): Add component

---
+++ src/components/app/AppText/AppText.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { FONTS_ENUM, TAGS_ENUM } from './types';
+
+interface Props {
+  tag?: TAGS_ENUM;
+  font?: FONTS_ENUM;
+  size?: string;
+}
+
+const props = defineProps<Props>();
+
+const classes = computed(() => {
+  const defaultTextClass = props.size ?? 'text-default';
+  const defaultTextFont = props.font ?? FONTS_ENUM.BODY;
+
+  return ['text', defaultTextClass, defaultTextFont];
+});
+</script>
+
+<template>
+  <component
+    :is="tag ?? 'p'"
+    :class="classes"
+  >
+    <slot />
+  </component>
+</template>
+
+<style scoped>
+.text-extra-large {
+  @apply text-5xl;
+}
+.text-large {
+  @apply text-3xl;
+}
+.text-medium {
+  @apply text-xl;
+}
+.text-regular {
+  @apply text-lg;
+}
+.text-default {
+  @apply text-base;
+}
+.text-small {
+  @apply text-sm;
+}
+
+.font-title {
+  @apply font-['Murecho'] font-bold;
+}
+
+.font-body {
+  @apply font-['Murecho'];
+}
+</style>
---
+++ src/components/app/AppText/types.ts
@@ -0,0 +1,20 @@
+enum TAGS_ENUM {
+  H1 = 'h1',
+  H2 = 'h2',
+  H3 = 'h3',
+  H4 = 'h4',
+  P = 'p',
+  SPAN = 'span',
+}
+
+enum FONTS_ENUM {
+  TITLE = 'font-title',
+  BODY = 'font-body',
+}
+
+const AppTextProps = {
+  FONTS_ENUM,
+  TAGS_ENUM,
+};
+
+export { AppTextProps, FONTS_ENUM, TAGS_ENUM };
